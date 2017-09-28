2debc53b6368f839a36b08e77489cb813377e618
Pande <pandeadhistanaya@gmail.com>
2017-09-29 00:10:53 +0700

    tambah rule pen[cdjz] aturan pemisah kata dengan imbuhan pen

--- tokenizer.py
+++ tokenizer.py
@@ -1,20 +1,15 @@
 import re
-#re = regex = regular expression
-#ignorcase mengabaikan huruf kapital
-#multiline mengabaikan enter/perpindahan baris
 
-TEXT = """Kami               belajar tanpa lelah.
-Kami& tidur tak teratur.
-Kami kuliah dengan giat.
-Kami korbankan masa#muda.
-Itu karena kami ingin kelak istri dan anak kami bahagia.
-ada     tikus"""
+rule1 = r'pen([cdjz])(.*)'
 
-def tokenize(text):
-    text = text.lower()
-    text = re.sub(r'[^a-z0-9 -]', ' ', text, flags = re.IGNORECASE|re.MULTILINE)# character sellain a-z, 0-9, " ", - digaanti dengan spasi kosong
-    text = re.sub(r'( +)', ' ', text, flags = re.IGNORECASE|re.MULTILINE)# ( +) menandakan spasi berlebih akan diubah menjadi satu spasi saja
-    tokens = text.split(" ") 
-    return tokens
+def stemmer(word, pattern):
+    ganti = ''
+    matched = re.match(rule1, word)
+    if matched:
+        return matched.group(1) + matched.group(2)
+    return word
 
-print(tokenize(TEXT))
+words = ['dimakan', 'dibuat', 'dirasakan', 'pendahulu', 'penjarah', 'penzazah', 'peniru']
+
+for word in words:
+    print(stemmer(word, rule1))
