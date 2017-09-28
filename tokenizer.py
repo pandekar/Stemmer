016c4b6ec4aaabeaa9bee57a5d57bc3eb10c46b5
Pande <pandeadhistanaya@gmail.com>
2017-09-28 23:20:32 +0700

    tokenizer di tambah regular expression

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,15 @@
-TEXT = """Kami belajar tanpa lelah."""
+import re
+
+TEXT = """Kami belajar tanpa lelah.
+Kami& tidur tak teratur.
+Kami kuliah dengan giat.
+Kami korbankan masa#muda.
+Itu karena kami ingin kelak istri dan anak kami bahagia."""
 
 def tokenize(text):
     text = text.lower()
-    tokens = text.split(" ")
+    text = re.sub(r'[^a-z0-9 -]', ' ', text, flags = re.IGNORECASE|re.MULTILINE)
+    tokens = text.split(" ") 
     return tokens
 
 print(tokenize(TEXT))
